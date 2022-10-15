00d755e0502b2929176fb1ff017ed4732173f493
Jayeshkhandelwal09 <khandelwaljayesh10@gmail.com>
2022-10-15 22:38:17 +0530

    Variables

---
+++ variable-and-datatypes-610 (1)/p3.js
@@ -0,0 +1,23 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+
+var name = "Jayesh";
+var school = "IIT bombay";
+var grade = "A+";
+var section = "A1";
+var roll_number = 18;
+var marks = "English = 98";
+var marks1 = "science = 100"
+var marks2 = "Maths = 100"
+
+
+console.log(name)
+console.log(school)
+console.log(grade)
+console.log(section)
+console.log(roll_number)
+console.log(marks)
+console.log(marks1)
+console.log(marks2)
